b958d2e9fc05d95da95b66f5e2617e639c867f94
Lottipopps <122055716+Lottipopps@users.noreply.github.com>
2023-04-15 13:19:23 +0200

    Added a toggle nav bar to all pages and made it into a responsive hamburger menu

--- src/app.js
+++ src/app.js
@@ -1,3 +1,21 @@
+function showMenu(event) {
+  let hamburgerMenu = document.querySelector(".hamburger-menu");
+  let navBar = document.querySelector(".nav-bar");
+  hamburgerMenu.classList.toggle("active");
+  navBar.classList.toggle("active");
+}
+function hidemenu(event) {
+  let hamburgerMenu = document.querySelector(".hamburger-menu");
+  let navBar = document.querySelector(".nav-bar");
+  hamburgerMenu.classList.remove("active");
+  navBar.classList.remove("active");
+}
+let hamburgerMenu = document.querySelector(".hamburger-menu");
+hamburgerMenu.addEventListener("click", showMenu);
+let navBar = document.querySelector(".nav-bar");
+let navbarLinks = document.querySelectorAll(".navbar-link");
+navbarElements.forEach((n) => n.addEventListener("click", hideMenu));
+
 function showCakes(event) {
   event.preventDefault();
   let ageInput = document.querySelector("#age-input");
@@ -10,5 +28,6 @@ function showCakes(event) {
     alert("Sorry! You are too young to view these, see you in a few years. 😜");
   }
 }
+
 let naughtyCakes = document.querySelector("#nsfw");
 naughtyCakes.addEventListener("submit", showCakes);
